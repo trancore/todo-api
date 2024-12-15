8b423f3f4f9ed70fe0876aac8d071198c7299492
trancore <ki.program0915@gmail.com>
2024-12-15 21:44:48 +0900

    feature: update useApiClient composables

--- apps/front/todo-nuxtjs/src/composables/useApiClient.ts
+++ apps/front/todo-nuxtjs/src/composables/useApiClient.ts
@@ -16,21 +16,6 @@ const defineUseApiClient = <Endpoints extends Record<string, any>>() => {
       UseFetchOptions<OpenApiResponse<Endpoints, Endpoint, Method>>
     >,
   ) => {
-    const runtimeConfig = useRuntimeConfig();
-    const baseOptions = {
-      baseURL: runtimeConfig.public.apiBaseUrl,
-      headers: {
-        'Access-Control-Allow-Origin': runtimeConfig.public.applicationUrl,
-      },
-    } as UseFetchOptions<
-      OpenApiResponse<
-        Endpoints,
-        keyof Endpoints,
-        keyof Endpoints[keyof Endpoints]
-      >
-    >;
-    const optionsResult = { ...baseOptions, ...options };
-
     const requestUrl = computed(() => {
       // OpenAPIから取得しているEndpoint型がStringを継承していないため型チェックせざるを得ない
       if (typeof request !== 'string') {
@@ -38,21 +23,25 @@ const defineUseApiClient = <Endpoints extends Record<string, any>>() => {
         throw new TypeError('Request must be string.');
       }
 
-      if ('params' in optionsResult) {
+      if ('params' in options) {
         return request.replace(/\{(.*?)\}/g, (matched, $1) => {
-          return optionsResult.params?.value[$1] || matched;
+          const params = options.params as Record<string, any>;
+          // options.paramsに値を入れたままにしてしまうとqueryに入ってしまうため、空文字列を入れる
+          options.params = '';
+          return params[$1] || matched;
         });
       }
       return request;
     });
-    const method = computed(() => optionsResult.method);
+    const method = computed(() => options.method);
 
-    const { data, error } = await useFetch(requestUrl.value, {
-      ...optionsResult,
+    const { data, error, execute } = await useFetch(requestUrl.value, {
+      ...options,
       method,
+      $fetch: useNuxtApp().$api,
     });
 
-    return { data, error };
+    return { data, error, execute };
   };
 };
 

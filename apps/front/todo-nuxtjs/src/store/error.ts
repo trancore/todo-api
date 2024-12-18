8f33077bc454e3e479e22059aef06663b6932807
trancore <ki.program0915@gmail.com>
2024-12-19 00:06:05 +0900

    feature: add error store

---
+++ apps/front/todo-nuxtjs/src/store/error.ts
@@ -0,0 +1,20 @@
+﻿export const useErrorStore = defineStore('errorStore', () => {
+  const errorMessage = ref('');
+  const isOpen = ref(false);
+
+  function setMesasge(message: string) {
+    errorMessage.value = message;
+  }
+
+  function openErrorMessage(message: string) {
+    setMesasge(message);
+    isOpen.value = !isOpen.value;
+  }
+
+  function clearErrorMessage() {
+    setMesasge('');
+    isOpen.value = !isOpen.value;
+  }
+
+  return { isOpen, openErrorMessage, clearErrorMessage };
+});
